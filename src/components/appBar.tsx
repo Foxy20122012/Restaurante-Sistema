0bd891a833656e397403d1ddfd619cb252b7c845
Foxy20122012 <malvarezm26@miumg.edu.gt>
2023-12-30 01:40:38 -0600

    preparacion para dezpliegue de prueba

--- src/components/appBar.tsx
+++ src/components/appBar.tsx
@@ -1,22 +1,37 @@
-'use client'
+// Importa las bibliotecas y componentes necesarios
+import React, { useState } from 'react';
+import Box from '@mui/material/Box';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import { AiOutlinePicRight } from 'react-icons/ai';
+import Sidebar from './Sidebar'; // Ajusta la ruta de importación según la estructura de tu proyecto
+import dynamic from 'next/dynamic';
 
-import React, { useState } from 'react'
-import Box from '@mui/material/Box'
-import AppBar from '@mui/material/AppBar'
-import Toolbar from '@mui/material/Toolbar'
-import Typography from '@mui/material/Typography'
-import Button from '@mui/material/Button'
-import { BiUserCircle } from 'react-icons/bi'
-import { AiOutlinePicRight } from 'react-icons/ai'
-import Sidebar from './Sidebar' // Ajusta la ruta de importación según la estructura de tu proyecto
-import Image from 'next/image'
+// Importa el componente UserMenu dinámicamente
+const UserMenu = dynamic(() => import("vComponents/dist/UserMenu"), { ssr: false });
 
+// Define y exporta el componente MainLayout
 const MainLayout = () => {
-  const [showSidebar, setShowSidebar] = useState(true)
+  const [showSidebar, setShowSidebar] = useState(true);
 
+  // Función para alternar la visibilidad de la barra lateral
   const handleToggleSidebar = () => {
-    setShowSidebar(!showSidebar)
-  }
+    setShowSidebar(!showSidebar);
+  };
+
+  // Función para manejar el clic en el botón de perfil del usuario
+  const handleProfileClick = () => {
+    // Aquí puedes implementar la lógica para redirigir al usuario a su perfil o hacer cualquier otra acción necesaria
+    console.log('Clicked on Profile');
+  };
+
+  // Función para manejar el clic en el botón de cerrar sesión
+  const handleLogoutClick = () => {
+    // Implementa la lógica para cerrar la sesión del usuario
+    console.log('Clicked on Logout');
+  };
 
   return (
     <Box sx={{ display: 'flex' }}>
@@ -34,15 +49,26 @@ const MainLayout = () => {
             <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
               Transportes Perdomo
             </Typography>
+            {/* Integra el componente UserMenu y pasa las props necesarias */}
             <Button color="inherit">
-              {/* <BiUserCircle size={24} style={{ marginRight: '16px' }} /> */}
-              <Image src={ 'nova.jpeg' } alt="logo" width={ 50 } height={ 50 } />
+            
+              <UserMenu
+              // @ts-ignore
+                userObj={{ 
+                  nombre_usuario: 'Transportes Perdomo', 
+                  compania: 'Eslogan', 
+                  email: 'transportesPerdono@transportesPerdomo.com' 
+                }}
+                onClickProfile={handleProfileClick}
+                onClickLogout={handleLogoutClick}
+                buttonStyles="bg-opacity-20 px-4 py-2 text-sm font-medium hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
+              />
             </Button>
           </Toolbar>
         </AppBar>
       </Box>
     </Box>
-  )
-}
+  );
+};
 
-export default MainLayout
+export default MainLayout;
